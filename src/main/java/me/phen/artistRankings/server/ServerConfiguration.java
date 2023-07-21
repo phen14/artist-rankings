e7e8f987ccbabf1c0c02792405fb5c048fa13c4d
Patrick W. Henstebeck <pwh@phen.me>
2023-07-21 01:41:13 -0700

    Revert "Add file"
    This reverts commit 0dc0c2f56d613280330f484d3a4c0c89a33b2fad.

--- src/main/java/me/phen/artistRankings/server/ServerConfiguration.java
+++
@@ -1,9 +0,0 @@
-// (K) ALL RIGHTS REVERSED - Reprint what you like
-
-package me.phen.artistRankings.server;
-
-import io.dropwizard.Configuration;
-
-public class ServerConfiguration extends Configuration {
-
-}
